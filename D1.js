8e2ead8f881b28caddf7c486ef4a3183b2a71d61
chiarasanasi <chiarasanasi.work@gmail.com>
2025-02-10 17:25:50 +0100

    fix extra

--- D1.js
+++ D1.js
@@ -76,4 +76,6 @@ let name1 = "john"
 let name2 = "John"
 
 console.log(name1 == name2)
-console.log(name1 !== name2)
+console.log(name1 == name2)
+
+console.log(name1.toLowerCase() === name2.toLowerCase())
